83223615dd4445e810b3a7dd78dc518a48d32ede
ibr03 <ibrahimforcode@gmail.com>
2023-10-08 19:46:45 +0530

    change token expiresIn

--- server/utils/SecretToken.js
+++ server/utils/SecretToken.js
@@ -2,6 +2,6 @@ const jwt = require("jsonwebtoken");
 
 module.exports.createSecretToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_KEY, {
-    expiresIn: 3 * 24 * 60 * 60,
+    expiresIn: '8h',
   });
 };
